d9075db6e4ee5aa9d73d01596e3dd2d9b6ac7078
Mesbahi Houda <98913666+Huselvp@users.noreply.github.com>
2023-10-16 01:31:46 -0700

    fix reset-password

--- src/main/java/com/iker/Lexly/Auth/AuthController.java
+++ src/main/java/com/iker/Lexly/Auth/AuthController.java
@@ -55,7 +55,6 @@ public class AuthController {
         if (!isTokenValid) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid or expired token");
         }
-
         User user = resetTokenService.getUserFromToken(token);
         if (user == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
