27f0c5382b51b14dcb3262e100b57d4fc0ccf5e6
Mesbahi Houda <98913666+Huselvp@users.noreply.github.com>
2023-11-02 04:07:20 -0700

    updates in user apis

--- src/main/java/com/iker/Lexly/Controller/suserController.java
+++ src/main/java/com/iker/Lexly/Controller/suserController.java
@@ -18,7 +18,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/suser")
 public class suserController {
 
     private final DocumentsService documentsService;
@@ -36,16 +36,6 @@ public class suserController {
         this.questionService = questionService;
         this.templateService = templateService;
     }
-
-    @GetMapping("/all_templates")
-    public List<TemplateDTO> getAllTemplates() {
-        List<Template> templates = templateService.getAllTemplates();
-        List<TemplateDTO> templateDTOs = templates.stream()
-                .map(templateTransformer::toDTO)
-                .collect(Collectors.toList());
-        return templateDTOs;
-    }
-
     @PostMapping("/create_document")
     public DocumentsDTO createDocument(@RequestBody DocumentCreateRequest documentCreateRequest) {
         return documentsService.createDocument(documentCreateRequest);
--- src/main/java/com/iker/Lexly/Controller/userController.java
+++ src/main/java/com/iker/Lexly/Controller/userController.java
@@ -1,29 +1,32 @@
 package com.iker.Lexly.Controller;
 
+import com.iker.Lexly.DTO.TemplateDTO;
+import com.iker.Lexly.Entity.Template;
+import com.iker.Lexly.Transformer.TemplateTransformer;
 import com.iker.Lexly.repository.RoleRepository;
+import com.iker.Lexly.service.TemplateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/api/user")
 public class userController {
     @Autowired
     RoleRepository roleRepository;
-    @GetMapping
-    public String get(){
-        return "GET :: suser controller";
-    }
-    @PostMapping
-    public String post(){
-        return "GET :: suser controller";
-    }
-    @PutMapping
-    public String put(){
-        return "GET :: suser controller";
-    }
-    @DeleteMapping
-    public String Delete(){
-        return "GET :: suser controller";
+    @Autowired
+    TemplateService templateService;
+    @Autowired
+    TemplateTransformer templateTransformer;
+    @GetMapping("/user_all_templates")
+    public List<TemplateDTO> getAllTemplates() {
+        List<Template> templates = templateService.getAllTemplates();
+        List<TemplateDTO> templateDTOs = templates.stream()
+                .map(templateTransformer::toDTO)
+                .collect(Collectors.toList());
+        return templateDTOs;
     }
 }
