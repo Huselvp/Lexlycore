f1868f5dd7128ae2e491e681947e8354a1790bfc
Mesbahi Houda <98913666+Huselvp@users.noreply.github.com>
2023-12-14 07:46:57 -0800

    logout process

--- src/main/java/com/iker/Lexly/Auth/AuthController.java
+++ src/main/java/com/iker/Lexly/Auth/AuthController.java
@@ -1,14 +1,18 @@
 package com.iker.Lexly.Auth;
 import com.iker.Lexly.Entity.User;
 import com.iker.Lexly.ResetSecurity.ResetTokenService;
+import com.iker.Lexly.Token.TokenRepository;
 import com.iker.Lexly.repository.UserRepository;
 import com.iker.Lexly.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,6 +35,8 @@ public class AuthController {
     private  final AuthenticationService service;
     @Autowired
     private final UserRepository userRepository;
+    @Autowired
+    private final TokenRepository tokenRepository;
 
 
     @PostMapping("/register")
@@ -86,4 +92,27 @@ public class AuthController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid or expired token");
         }
     }
+    @PostMapping("/logout")
+    @Transactional
+    public void logout(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Authentication authentication
+    ) {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return;
+        }
+        jwt = authHeader.substring(7);
+        var storedToken = tokenRepository.findByToken(jwt).orElse(null);
+        if (storedToken != null) {
+            storedToken.setExpired(true);
+            storedToken.setRevoked(true);
+            tokenRepository.save(storedToken);
+            tokenRepository.deleteByToken(jwt);
+            SecurityContextHolder.clearContext();
+        }
+    }
+
 }
