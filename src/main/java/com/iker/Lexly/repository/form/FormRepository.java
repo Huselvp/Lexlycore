3ea885f62b465d67bb67a708ad4a668b84c25f41
kawtar <kawtar.karkour@gmail.com>
2024-05-20 15:11:06 +0100

    add form repository

---
+++ src/main/java/com/iker/Lexly/repository/form/FormRepository.java
@@ -0,0 +1,6 @@
+package com.iker.Lexly.repository.form;
+import com.iker.Lexly.Entity.Form.Form;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FormRepository extends JpaRepository<Form, Long> {
+}
